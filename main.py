296e763c5f61d59896d2231c021beea7f5cf4d98
CosmicRadiocity <heartbreakersaturn@gmail.com>
2025-02-10 16:51:54 +0100

    word count function

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("hello world")
+def main():
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+        print(file_contents)
+        words = word_count(file_contents)
+        print(f"Words: {words}")
+
+def word_count(txt):
+    words = txt.split()
+    return len(words)
+
+main()
